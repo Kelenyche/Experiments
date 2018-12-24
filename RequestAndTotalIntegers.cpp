0dd4fd73cd0aa5caec8341a439ea3eb58814e4d7
meriamdelerium <46062421+meriamdelerium@users.noreply.github.com>
2018-12-23 18:43:31 -0900

    Add files via upload
    First request and then use data.

---
+++ RequestAndTotalIntegers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int firstInt, secondInt, sum;
+int main()
+{	cout <<"Enter two integers, separated by a space: ";
+	cin >>firstInt >>secondInt;
+	sum = firstInt + secondInt;
+	cout <<"The sum is: " <<sum <<endl;
+}
